e8250d9e9cd07db1ff9d988f134bdce474700496
Breno Santos Tracci <133373003+Shyaldir@users.noreply.github.com>
2023-05-15 08:47:23 -0300

    Add files via upload

---
+++ Teste_Base.js
@@ -0,0 +1,38 @@
+function Mensagem(){
+    alert("Você será redirecionado para o seu Servidor de Email. Caso não esteja logado no seu Email, efetue o Login e tete novamente!")
+}
+
+function click_menu(){
+    if(Itens.style.display == 'block'){
+        Itens.style.display = 'none'
+    }else{
+        Itens.style.display = 'block'
+    }
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active";
+}
